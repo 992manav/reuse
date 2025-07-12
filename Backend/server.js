617bedc84d7c6ebf51b0f7b4e259ef735417cbb0
992manav <dmanavpianist@gmail.com>
2025-07-12 10:36:47 +0530

    auth and databse connect

--- Backend/server.js
+++ Backend/server.js
@@ -2,8 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
-import userRoutes from "./routes/userRoutes.js";
-import itemRoutes from "./routes/itemRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+// import userRoutes from "./routes/userRoutes.js";
+// import itemRoutes from "./routes/itemRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -12,10 +13,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/users", userRoutes);
-app.use("/api/items", itemRoutes);
+// Routes
+app.use("/api/auth", authRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/items", itemRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(` Server running on http://localhost:${PORT}`);
 });
